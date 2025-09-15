c88db314c997b2cf28ee349deb3dedaaa95aa3fd
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-09-15 12:09:43 +0300

    login-checked

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) {}
+    async validateUser( email: string, password: string) {
+        const user = this.usersService.findByEmail(email);
+        if (!user) return null;
+
+        if(user.password !== password) return null;
+
+        return user;
+    }
+    async login(email: string, password: string) {
+        const user = await this.validateUser(email, password);
+        if (!user) throw new UnauthorizedException('Invalid credentials');
+
+        const payload = { sub: user.id, email: user.email };
+        return {access_token: this.jwtService.sign(payload) };
+    }
+}
