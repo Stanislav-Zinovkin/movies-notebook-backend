e6e82c4d15fc21235bb011444e1bcd76bf8e27be
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-09-23 17:16:53 +0300

    fixed-paths-to-files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from 'prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [UsersModule, AuthModule, PrismaModule],
